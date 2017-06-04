7cdd081606f27991b935c914f96544f16d865dec
MrKAKTyC <maximus.ukr@gmail.com>
2017-06-04 14:05:05 +0300

    Basic
    Add basic files

---
+++ SuClient.java
@@ -0,0 +1,10 @@
+package ClientPackage;
+
+public class SuClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SuServer.java
@@ -0,0 +1,10 @@
+package ServerPackage;
+
+public class SuServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
